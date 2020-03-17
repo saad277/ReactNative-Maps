c4a956af1e44c1168c3d6fb292621427d13287cd
saad277 <saadsheikh27@gmail.com>
2020-03-17 18:57:49 +0500

    map working

---
+++ App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import MapView from 'react-native-maps'
+
+
+
+const App = () => {
+
+
+  return (
+
+    <View style={styles.container}>
+
+
+      <MapView
+        // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      >
+      </MapView>
+    </View>
+  )
+
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+
+
+})
+
+
+export default App;
